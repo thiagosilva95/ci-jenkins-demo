c1c0fd4316e70aa1c875ad1b3ddea9b326e0c32e
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-05-26 17:58:46 -0300

    Initial commit

---
+++ src/main/java/com/tdev/example/cijenkinsdemo/CiJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tdev.example.cijenkinsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiJenkinsDemoApplication.class, args);
+	}
+
+}
